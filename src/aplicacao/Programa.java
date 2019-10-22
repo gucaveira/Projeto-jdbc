d34677c76753efae014f46bdab15c86a66768f67
gucaveira <pereiragustavo215@gmail.com>
2019-10-22 14:06:22 -0300

    Criando classe Departamentoe

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,11 @@
+package aplicacao;
+
+import model.entidade.Departamento;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        Departamento obj = new Departamento(1, "Livros");
+        System.out.println(obj);
+    }
+}
